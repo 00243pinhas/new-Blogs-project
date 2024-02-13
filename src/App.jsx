7b7a487942c3c4c7c7a6c5a6fef407fbaa2d8d03
00243pinhas <pinaslok@gmail.com>
2024-02-13 20:18:11 +0300

    disclamer settupps

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,27 @@ import Create  from "./componentes/create/create.jsx"
 import BlogDetails from "./componentes/blogs-details/BlogDetails.jsx"
 import { BrowserRouter as Router, Route , Switch } from "react-router-dom/cjs/react-router-dom.min.js"
 import NotFound from "./componentes/notFound.jsx"
+import Disclamer from "./componentes/disclamer/disclamer.jsx"
+import './App.css'
 
 
 function App() {
   return (
     <>
       <Router>
-
       <div className="app"></div>
           <NavBar/> 
+
+          <div className="disclamerWrapper">
+            <Disclamer/>
+          </div>
+
         <div className="content">
            <Switch>
 
+          
             <Route exact path='/'>
+     
               <Home/>
             </Route>
 
---
+++ src/componentes/disclamer/disclamer.jsx
@@ -0,0 +1,13 @@
+import '../disclamer/disclamer.css'
+
+export default function Disclamer() {
+    return (
+  
+      <div className="disclamer"> 
+            <p>
+            Please note: Due to limitations on the hosting platform, this JSON API is read-only,
+             preventing POST and DELETE methods. Thank you for your understanding.
+            </p>
+      </div>
+    )
+  }
