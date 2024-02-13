77c1be1957efb7440fd9546359e7ec0f1e2cfa27
00243pinhas <pinaslok@gmail.com>
2024-02-13 18:02:16 +0300

    git last changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // Set the base path to the current directory
+  base: './', 
   plugins: [react()],
   resolve: {
     alias: {
