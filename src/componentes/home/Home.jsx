d84d938c69e46d7e0c337bdc3f565a3a032f374b
00243pinhas <pinaslok@gmail.com>
2024-06-06 22:18:10 +0300

    components tours

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -2,60 +2,35 @@ import '../home/home.css'
 
 import { useState , useEffect} from 'react'
 
-import BlogList from '../blogList'
 
-import useFetch from '../useFetch'
 
 
 export default function Home() {
 
-  const [title, settitle]=useState("");
-  const [author, setAuthor]=useState("");
-  const [body,setbody]=useState("");
-  
-  
-    const handleSubmit=(event) => {
-      event.preventDefault();
+  const [blogs, setBlogs] = useState([]);
 
-      const newblogs={title,author,body};
 
-      fetch('http://127.0.0.1:5000/blogs', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newblogs),
-      })
-        .then(response => response.json())
-        .then(data => console.log('Blog created:', data))
-        .catch(error => console.error('Error creating blog:', error));
-    };
+  useEffect(()=>{
 
-  
+      fetch('http://127.0.0.1:5000/blogs')
+      .then(response => response.json())
+      .then(data => setBlogs(data))
+      .catch(error => console.error('Error fetching blogs:', error));
+
+  },[]);
+
   return (
-    <form onSubmit={handleSubmit}>
 
-      <input type="text" value={title} 
-      onChange={(e) => settitle(e.target.value)}
-       placeholder="title" 
-       required 
-      />
-
-      <input type="text" value={author} 
-      onChange={(e) => setAuthor(e.target.value)}
-       placeholder="Author" 
-       required 
-      />
-
-      <input type="text"
-          value={body}
-          onChange={(e)=>setbody(e.target.value)}
-          placeholder='body authore'
-          required
-       />
-
-      <button type="submit">Create Blog</button>
-    </form>
+    <div>
+      
+    {blogs.map((blog) => (
+            <div key={blog.id}>
+              <h2>{blog.title}</h2>
+              <p>By {blog.author}</p>
+              <p>{blog.body}</p>
+            </div>
+          ))}
+    </div>
   )
 
 }
