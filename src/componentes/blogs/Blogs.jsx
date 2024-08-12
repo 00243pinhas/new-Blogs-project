eb72dac2208388d3d4a49b94683e49d844d00d6a
00243pinhas <pinaslok@gmail.com>
2024-08-12 16:49:35 +0300

    Category brn Done

--- src/componentes/blogs/Blogs.jsx
+++ src/componentes/blogs/Blogs.jsx
@@ -8,7 +8,18 @@ import "./blogs.css";
 export default function Blogs() {
 
   const {blogs, isPending,error}= useFetchBlogs();
+  const [selectedCategory, setSelectedCategory]= useState ("All");
 
+  const categories = ["All", "Sport", "Love", "Politics", "Social", "Economic"];
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filterBlogs = 
+    selectedCategory ==='All'
+    ?blogs
+    : blogs.filter((blog)=>blog.category === selectedCategory)
   if (isPending){
     return <div>Loading...</div>;
   }
@@ -20,50 +31,74 @@ export default function Blogs() {
   return (
     <>
 
-    <div className="contentErapper">
+    <div className="contentWrapper">
                    
+        
+
             <div className="flex flex-wrap justify-center mb-9 gap-5">
-
-                <Button gradientMonochrome="success">Sport </Button>
-                <Button gradientMonochrome="success">Love</Button>
-                <Button gradientMonochrome="success">Politics</Button>
-                <Button gradientMonochrome="success">Social</Button>
-                <Button gradientMonochrome="success">Economics</Button>
-
+              {categories.map((category) => (
+                <Button
+                  key={category}
+                  gradientMonochrome="success"
+                  onClick={() => handleCategoryClick(category)}
+                >
+                  {category}
+                </Button>
+              ))}
             </div>
-              {blogs.map((item) => (
-                <div className="blog-card" key={item.id}> 
+              
+           
+
+            
+            {filterBlogs.length > 0 ? (
+              filterBlogs.map((item) => (
+                <div className="blog-card" key={item.id}>
                   <div className="meta">
-                    <div 
-                      className="photo" 
+                    <div
+                      className="photo"
                       style={{ backgroundImage: `url(${item.image})` }}
                     ></div>
                     <ul className="details">
-                      <li className="author"><a href="#">{item.author}</a></li>
+                      <li className="author">
+                        <a href="#">{item.author}</a>
+                      </li>
                       <li className="date">Aug. 24, 2015</li>
                       <li className="tags">
                         <ul>
-                          <li><a href="#">Learn</a></li>
-                          <li><a href="#">Code</a></li>
-                          <li><a href="#">HTML</a></li>
-                          <li><a href="#">CSS</a></li>
-                        </ul> 
+                          <li>
+                            <a href="#">Learn</a>
+                          </li>
+                          <li>
+                            <a href="#">Code</a>
+                          </li>
+                          <li>
+                            <a href="#">HTML</a>
+                          </li>
+                          <li>
+                            <a href="#">CSS</a>
+                          </li>
+                        </ul>
                       </li>
                     </ul>
                   </div>
                   <div className="description">
                     <h1>{item.title}</h1>
                     <h2>{item.category}</h2>
-                        <div className="body_contents">
-                            <p>{item.body}</p>
-                        </div>
+                    <div className="body_contents">
+                      <p>{item.body}</p>
+                    </div>
                     <p className="read-more">
                       <a href="#">Read More</a>
                     </p>
                   </div>
                 </div>
-              ))}
-    </div>
+              ))
+            ) : (
+              <div>No blogs available for the selected category.</div>
+            )}
+          </div>
+        
+      
  
     </>
   );
