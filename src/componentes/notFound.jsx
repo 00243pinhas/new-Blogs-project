b92e9c63c2d17075586ffc6d1ca98958f8662321
00243pinhas <pinaslok@gmail.com>
2024-08-27 01:22:51 +0300

    cleanning up

--- src/componentes/notFound.jsx
+++ src/componentes/notFound.jsx
@@ -3,7 +3,14 @@
 export default function NotFound() {
   return (
 
-    <div>Huh!! You are Lost Completely...</div>
+  <div className="grid justify-center align-center mt-[10rem] bg-bleu gap-[4rem]">
+
+    <div className="flex justify-center">
+      <h1 className="text-4xl text-gray font-costum font-bold">we are lost</h1>
+    </div>
+    <div className="illutraction">      <img src="src/assets/undraw_Lost_re_xqjt.png" alt=""  className='w-full h-auto object-cover rounded-lg max-w-xs sm:max-w-sm md:max-w-md lg:max-w-5xl' />
+    </div>
+  </div>
   )
 }
 
